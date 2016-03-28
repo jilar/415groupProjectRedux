ee96453acbee4f0bfb9d55cefad1ec956fdba56d
jilar <j.ilar1215@hotmail.com>
2016-03-28 13:08:14 -0700

    Added files via upload
    I goofed

--- Directory.js
+++ Directory.js
@@ -1,12 +1,12 @@
-var Directory = {
-  Files: [],
-}
+//var Directory = {
+//  Files: [],
+//}
 
-////Directory of current files and processes
-//var Directory = [
-//   "bankBook.csv","ContactManager.csv","securityFile.csv","route.csv","vectorData.csv","statsFile.csv"
-//]
-//
-//var ProcessNames= [
-//    bankCalculator, contactManager, securityUpdate, shortestRoute, addVectors, calculateStats
-//]
+//Directory of current files and processes
+var Directory = [
+   "bankBook.csv","ContactManager.csv","securityFile.csv","route.csv","vectorData.csv","statsFile.csv"
+]
+
+var ProcessNames= [
+    bankCalculator, contactManager, securityUpdate, shortestRoute, addVectors, calculateStats
+]
